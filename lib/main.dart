b137e1e088a1e95a7a3a6b7a5e5161869769cb56
Md Abu Nayeb Towki <nybtwki98@gmail.com>
2022-01-22 14:50:12 +0000

    Dicee-FlutterAppDevelopment-Practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +15,41 @@ void main() {
     ),
   );
 }
-
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber= 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace(){
+    setState(() {
+      rightDiceNumber = Random().nextInt(6)+1;
+      leftDiceNumber = Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+               changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),),
+          ),
+        ],
+      ),
+    );
   }
 }
